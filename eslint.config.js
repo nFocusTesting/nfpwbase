926d0e4f5923c572e72e886735755235b9c5d649
SwederellNF <137289156+SwederellNF@users.noreply.github.com>
2024-04-29 20:33:36 +0100

    update tsconfig, eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,28 @@
+// @ts-check
+
 import eslint from "@eslint/js";
+import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   {
-    files: ['**/*.ts'],
-    ignores: ["*.config.js", './node_modules', './playwright-report', 'test-results'],
+    files: ["**/*.js"],
+    ignores: ["node_modules/", "playwright-report/", "test-results/"],
+    ...tseslint.configs.disableTypeChecked,
+  },
+  {
+    files: ["**/*.ts"],
+    ignores: ["node_modules/", "playwright-report/", "test-results/"],
     languageOptions: {
-      globals: {},
       parserOptions: {
-        project: true,
-        tsconfigRootDir: import.meta.dirname,
+        project: "./tsconfig.json",
       },
+      globals: { ...globals.node },
     },
     rules: {
+      "@typescript-eslint/no-floating-promises": ["error"],
       "@typescript-eslint/restrict-template-expressions": "off",
       "@typescript-eslint/no-base-to-string": [
         "warn",
